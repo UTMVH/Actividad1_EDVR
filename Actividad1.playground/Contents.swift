3cd4297c3d209aad908ea02fe3e162928b6f1161
Eduardo Vazquez <>
2021-08-18 23:50:16 -0500

    Tarea 1

---
+++ Actividad1.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+//:Asignación de valor mutable
+var v1 = "esto es Swift"
+ 
+ 
+
+
+
+
+//:Asignación de valor inmutable
+ let c1 = 4
+
+//: Asignación formal (definiendo el tipo de dato)
+var cadena:String = String()
+
+cadena = "Hola mundo " + v1
+print("El contenido es: \(cadena)")
+print("El valor de constente es: \(c1)")
+
+
+
+
+
